aa9539f14f66b349eea25f6db5764a55cb43f7c7
Alexandr <81583071+Alexandr2106@users.noreply.github.com>
2022-03-12 16:15:36 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
           <div class="col-md-4 logo">
             <div class="custom">
               <img src="img/logo.svg" class="img-fluid" alt="Logo">
+              <img src="img/reh-shapka.png" class="img-fluid" alt="Logo">
             </div>
           </div>
 
@@ -134,7 +135,7 @@
 
     <!--******************************************-->
     <div class="container-fluid slider">
-      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -145,11 +146,11 @@
             <img src="img/slide1.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item" data-bs-interval="5000">
-            <img src="img/slide1.jpg" class="d-block w-100" alt="...">
+            <img src="img/slide2.jpg" class="d-block w-100" alt="...">
 
           </div>
           <div class="carousel-item" data-bs-interval="5000">
-            <img src="img/slide1.jpg" class="d-block w-100" alt="...">
+            <img src="img/slide3.jpg" class="d-block w-100" alt="...">
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -167,22 +168,175 @@
 
     <main class="container-fluid" style="padding: 0;">
 
-      <div class="container-fluid form-bg" style="height: 475px; background: url(img/form-bg.jpg); background-size: cover;
+      <div class="container-xxl works">
+
+        <div class="block-title">Наши работы</div>
+
+        <!--Big Carousel-->
+        <div id="carouselExampleControls" class="carousel slide big-carousel" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="card-group">
+                <div class="card">
+                  <img src="img/exaple1.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/exaple2.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/exaple1.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="card-group">
+                <div class="card">
+                  <img src="img/exaple1.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/exaple2.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/exaple1.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="card-group">
+                <div class="card">
+                  <img src="img/exaple1.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/exaple2.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+                <div class="card">
+                  <img src="img/exaple1.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Установка трехстворчатых окон</h5>
+                    <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Предыдущий</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Следующий</span>
+          </button>
+        </div>
+
+
+        <!--Smart Carousel-->
+        <div id="carouselExampleControls" class="carousel slide smart-carousel" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+
+              <div class="card">
+                <img src="img/exaple2.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Установка трехстворчатых окон</h5>
+                  <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                </div>
+              </div>
+
+            </div>
+            <div class="carousel-item">
+
+              <div class="card">
+                <img src="img/exaple1.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Установка трехстворчатых окон</h5>
+                  <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                </div>
+              </div>
+
+            </div>
+            <div class="carousel-item">
+
+              <div class="card">
+                <img src="img/exaple2.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Установка трехстворчатых окон</h5>
+                  <p class="card-text"><small class="text-muted">г. Москва, ул. Какая-то</small></p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Предыдущий</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Следующий</span>
+          </button>
+        </div>
+
+
+      </div>
+
+      <div class="container-fluid form-bg" style="min-height: 475px;; background: url(img/form-bg.jpg); background-size: cover;
     background-position-y: center;">
+        <div class="cont">
+          <div>
+            <div class="form-title">Вызвать замерщика бесплатно</div>
+            <form>
+              <div class="col form-subtitle">Оставьте свой номер телефона, и мы вам перезвоним</div>
 
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Адрес электронной почты</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div class="row">
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Имя*" aria-label="Имя">
+                </div>
+                <div class="col">
+                  <input type="tel" class="form-control" placeholder="Телефон" aria-label="Телефон" name="phone" pattern="\+7\s?[\(]{0,1}9[0-9]{2}[\)]{0,1}\s?\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}" required>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">Нажимая кнопку, вы соглашаетесь с условиями Политики конфиденциальности</div>
+                <div class="col button">
+                  <button type="submit" class="btn btn-primary"><img src="img/pen.svg" alt="Кнопка записаться на замер"> Записаться на замер</button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Пароль</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-          </div>
-
-          <button type="submit" class="btn btn-primary">Отправить</button>
-
-        </form>
+        </div>
       </div>
 
       <div id="pp">
